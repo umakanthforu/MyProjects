2acede3a9c106d5ca371ee9cda6ba0d8e0e15266
umakanthforu <kanth2004a@gmail.com>
2024-10-27 12:51:04 +0530

    Terraform has been added to Project

--- Pandas/Pandas_by_Saradhi/RoughWork.py
+++ Pandas/Pandas_by_Saradhi/RoughWork.py
@@ -1,4 +1,6 @@
 import pandas
+import boto3
+import s3fs
 
 resource = pandas.read_excel("Data Sets\\student_marks.xlsx", sheet_name = "markssheet_missing")
 
@@ -17,4 +19,30 @@ dataframe = pandas.DataFrame(resource)
 # dataframe.loc[(dataframe["Percentage"] >= 50) & (dataframe["Percentage"] <= 59), ["Grade"]]= "Average"
 # dataframe.loc[dataframe["Percentage"] <= 49, ["Grade"]]= "Below Average"
 # print(dataframe.sort_values("Percentage", ascending=False))
+# print(dataframe)
+dataframe.drop(columns="Admission No", inplace=True)
+# print(dataframe)
+dataframe.dropna(inplace=True)
+# print(dataframe)
+
+dataframe["Total"] = dataframe["English"]+dataframe["Hindi"]+dataframe["Telugu"]+dataframe["Maths"]+dataframe["Science"]+dataframe["Social"]
+
+dataframe.drop_duplicates(inplace=True)
+# print(dataframe)
+
+
+
+dataframe["Percentage"] = (dataframe["Total"]/600)*100
+dataframe["Grade"]= "To be calculated"
+# print(dataframe)
+
+# print(dataframe)
+
+dataframe.loc[dataframe["Percentage"] >= 60,["Grade"]] = "First Class..!"
+dataframe.loc[(dataframe["Percentage"] >= 50) & (dataframe["Percentage"] <= 59), ["Grade"]] = "Second Class"
+dataframe.loc[dataframe["Percentage"] <= 49, ["Grade"]] = "Failure.."
+# print(dataframe)
+
+dataframe.to_excel("C:\\Users\\kanth\\OneDrive\\Desktop\\New folder\\mydata.xlsx",index=False)
+
 
