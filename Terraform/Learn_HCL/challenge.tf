a839dcd2c93f826d6305b6db9d90040b0f7f00e6
UMAKANTH ACHUKOLU <106602686+umakanthforu@users.noreply.github.com>
2024-10-27 13:58:07 +0530

    Delete Terraform/Learn_HCL directory

--- Terraform/Learn_HCL/challenge.tf
+++
@@ -1,7 +0,0 @@
-# CHALLENGE : creat a file in local machine using terraform
-
-resource "null_resource" "name" {
-    provisioner "local-exec" {
-        command = "echo 'Message: ${upper("Hello World")}' > challenge.txt"
-        }
-    }
