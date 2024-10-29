d25b154b620e1b0f86e9061f7f683483328085f8
umakanthforu <kanth2004a@gmail.com>
2024-10-29 10:23:13 +0530

    Commited after finishing work

--- API handling Project/roughwork.py
+++ API handling Project/roughwork.py
@@ -1,13 +1,29 @@
-import requests
+# import requests
 
-url = "https://api.freeapi.app/api/v1/public/books/book/random"
+# url = "https://api.freeapi.app/api/v1/public/youtube/channel"
 
-response = requests.get(url)
+# response = requests.get(url)
 
-database = response.json()
+# masterdata = response.json()
 
-code = database.get("data")
-print(code)
+# def youtubedata():
+#     if masterdata["success"] and "data" in masterdata:
+#         url = masterdata["data"]["info"]["snippet"]["customUrl"]
+#         description = masterdata["data"]["info"]["snippet"]["description"]
+#         return url, description
+#     else:
+#         raise Exception("Some error has occured")
 
+# def main():
+#     try:
+#         yurl, ydescription = youtubedata()
+#         print(yurl)
+#         print(ydescription)
+#     except Exception as e:
+#         print(e)
 
-print("Name is umakanth")
+# if __name__ == "__main__":
+#     main()
+# else:
+#     pass
+
--- Managing_S3_using_BOTO3/Rough_work.py
+++ Managing_S3_using_BOTO3/Rough_work.py
@@ -1,8 +1,50 @@
 import boto3
+import s3fs
 import pandas
 
-rawdata = pandas.read_excel("student_marks.xlsx")
 
-dataframe = pandas.DataFrame(rawdata)
+s3_client = boto3.client(
+    service_name = "s3"
+)
 
-print(dataframe.size)
+# resource = s3_client.create_bucket(
+#     Bucket = "umakanth-achukolu-creation-2024",
+#     CreateBucketConfiguration = {
+#         "LocationConstraint" : "ap-south-1"
+#     }
+# )
+
+# presource = pandas.read_excel("Managing_S3_using_BOTO3\\student_marks.xlsx")
+
+# dataframe = pandas.DataFrame(presource)
+
+
+# dataframe["Total"]= dataframe["English"]+dataframe["Hindi"]+dataframe["Telugu"]+dataframe["Maths"]+dataframe["Science"]+dataframe["Social"]
+
+# dataframe.to_excel("C:\\Users\\kanth\\OneDrive\\Desktop\\Trial\\myfile.xlsx", index=False)
+
+# s3resource = s3_client.put_object(
+#     Body = str(dataframe),
+#     Bucket = "umakanth-achukolu-creation-2024",
+#     Key = "my_dataframe.xlsx"
+# )
+
+# resource = s3_client.get_object(
+#     Bucket = "umakanth-achukolu-creation-2024",
+#     Key = "my_dataframe.xlsx"
+# )
+
+# downloadfile = resource.get("Body").read().decode()
+# # print(downloadfile)
+
+# with open("downloadedfile_s3.xlsx", "w") as f:
+#     f.write(downloadfile)
+
+# resource = s3_client.delete_object(
+#     Bucket = "umakanth-achukolu-creation-2024",
+#     Key = "my_dataframe.xlsx"
+# )
+
+resource = s3_client.delete_bucket(
+    Bucket = "umakanth-achukolu-creation-2024"
+)
